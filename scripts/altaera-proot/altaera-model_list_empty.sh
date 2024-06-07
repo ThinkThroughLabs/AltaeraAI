c274a704c4a7632d1c044a1d6fa4c0dfd13dc7ac
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-06-07 17:05:18 +0200

    Create altaera-model_list_empty.sh

---
+++ scripts/altaera-proot/altaera-model_list_empty.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+dialog --backtitle "AltaeraAI" \
+--title "AltaeraAI - List Models" \
+--msgbox 'No models found locally' 25 40
+
+bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
