074aa47eedaebfaa6b1fbdb3c76751cf855c5a03
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-06-09 12:42:32 +0200

    Create altaera-fix-slow_no-blas.sh

---
+++ scripts/altaera-proot/altaera-fix-slow_no-blas.sh
@@ -0,0 +1,31 @@
+echo "Updating AltaeraAI core files...
+
+" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+
+{
+mv '/data/data/com.termux/files/home/AltaeraAI-temp' '/data/data/com.termux/files/home/AltaeraAI-tmp'
+
+
+mkdir '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-temporary'
+mv '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/kcpp-ae/models' '/data/data/com.termux/files/home/AltaeraAI-tmp'
+cp '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/kcpp-ae/prompts/chat-with-bob.txt' '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera-temporary'
+cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
+rm -rf 'upgrade.sh'
+wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/upgrade.sh
+chmod a+x 'upgrade.sh'
+rm -rf 'ngrok-authtoken.sh'
+cd 'kcpp-ae'
+rm -rf 'klite.embd'
+clear
+ if git pull | grep 'Already up to date.'; then
+          :
+          else
+          make
+        fi
+rm -rf 'models'
+rm -rf 'klite.embd'
+wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/klite/klite.embd
+      cd ..
+      cd 'kcpp-ae_cm'
+      rm -rf 'klite.embd'
+      wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/klite/klite.embd
