27dc15539cb986251c737172c96579622d3abe1a
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-06-09 12:50:56 +0200

    Create altaera-fixed_successfully.sh

---
+++ scripts/altaera-proot/altaera-fixed_successfully.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dialog --backtitle "AltaeraAI" \
+--title "AltaeraAI - Fix" \
+--msgbox 'AltaeraAI core files have been fixed succesfully!
+' 25 40
+
+exit
+bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
