a95295199eed7be34b5690e0b7f1353c6b29d179
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-06-09 15:26:06 +0200

    Update altaera-model_list_empty.sh

--- scripts/altaera-proot/altaera-model_list_empty.sh
+++ scripts/altaera-proot/altaera-model_list_empty.sh
@@ -2,7 +2,7 @@
 
 dialog --backtitle "AltaeraAI" \
 --title "AltaeraAI - List Models" \
---msgbox 'No models found locally' 25 40
+--msgbox 'No models found locally' 10 40
 
 exit
 bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
