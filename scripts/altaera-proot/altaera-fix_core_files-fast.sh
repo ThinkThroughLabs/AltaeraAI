b7f958f0f02a37a8d2f4961754f11f3fc91fa65f
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-06-09 13:54:37 +0200

    Update altaera-fix_core_files-fast.sh

--- scripts/altaera-proot/altaera-fix_core_files-fast.sh
+++ scripts/altaera-proot/altaera-fix_core_files-fast.sh
@@ -9,7 +9,7 @@ mv '/data/data/com.termux/files/home/AltaeraAI-temp' '/data/data/com.termux/file
 mkdir 'altaera-temporary'
 mv 'kcpp-ae/models' '/data/data/com.termux/files/home/AltaeraAI-tmp'
 cp 'kcpp-ae/prompts/chat-with-bob.txt' 'altaera-temporary'
-cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
+cd '~'
 rm -rf 'kcpp-ae'
 rm -rf 'upgrade.sh'
 wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/upgrade.sh
