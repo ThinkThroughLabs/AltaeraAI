982986f5d0b37e49d41a50d6495465d11c451bd2
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-06-17 12:21:49 +0200

    Update altaera-updated_successfully.sh

--- scripts/altaera-updated_successfully.sh
+++ scripts/altaera-updated_successfully.sh
@@ -7,12 +7,14 @@ dialog --backtitle "AltaeraAI" \
 
 Recent changes:
 
+    * v5.1.2 - fixed the issue regarding pre-packaged KoboldCpp not being downloaded after switching to an organisational repository
+    * added a pre-launch check (when starting KoboldCpp) to see if the KoboldCpp directory exists in PRoot Distro; if not, the user will be asked whether to download or compile it
+    * v5.1.1 - shifted the projects main GitHub repository into an organisational one (ThinkThroughLabs). This upgrade does not bring any functionalities, its sole purpose is to redirect local AltaeraAI update mechanisms to a new address
+    * v5.1 - updated to koboldcpp-1.67
+    * added "aef", "aeforce" and "altaeraforce" arguments to the "bash.bashrc" file, which allow the user to launch AltaeraAI without the automatic update checking mechanism, in case there is a start-up problem, i.e., poor network connectivity
     * v5.0 - updated to koboldcpp-1.66.1
     * visual fixes and improvements to Model MENUS
     * v4.9.6 - added Phi-SoSerious-Mini-V1/imatrix weights to the list of models
-    * v4.9.5 - fixed "KoboldCpp Settings"
-    * v4.9.4 - added an optional (set by default) black MENU background (Bash display dialog boxes)
-    * v4.9.3 – added Gemma-2B/7B-it weights (and a reference to their LICENSE file, with a notice) to the list of models
 ' 25 40
 
 bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
