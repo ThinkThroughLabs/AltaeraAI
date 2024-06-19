584b8b0d1e43c7b9b70c22f4eb110f4eb5e19827
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-06-19 15:41:57 +0200

    Update altaera-updated_successfully.sh

--- scripts/altaera-updated_successfully.sh
+++ scripts/altaera-updated_successfully.sh
@@ -7,14 +7,16 @@ dialog --backtitle "AltaeraAI" \
 
 Recent changes:
 
-    * v5.2 - fixed the issue regarding pre-packaged KoboldCpp not being downloaded after switching to an organisational repository
+    * v5.2 – updated to koboldcpp-1.68
+    * added the “Horde” option to the MENU, which utilises AI Horde to allow for sharing your processing power (an AI Model) for users worldwide
+    * minor aesthetic changes and fixes to the MENU
+    * v5.1.2 - fixed the issue regarding pre-packaged KoboldCpp not being downloaded after switching to an organisational repository
     * added a pre-launch check (when starting KoboldCpp) to see if the KoboldCpp directory exists in PRoot Distro; if not, the user will be asked whether to download or compile it
     * v5.1.1 - shifted the projects main GitHub repository into an organisational one (ThinkThroughLabs). This upgrade does not bring any functionalities, its sole purpose is to redirect local AltaeraAI update mechanisms to a new address
     * v5.1 - updated to koboldcpp-1.67
     * added "aef", "aeforce" and "altaeraforce" arguments to the "bash.bashrc" file, which allow the user to launch AltaeraAI without the automatic update checking mechanism, in case there is a start-up problem, i.e., poor network connectivity
     * v5.0 - updated to koboldcpp-1.66.1
     * visual fixes and improvements to Model MENUS
-    * v4.9.6 - added Phi-SoSerious-Mini-V1/imatrix weights to the list of models
 ' 25 40
 
 bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
