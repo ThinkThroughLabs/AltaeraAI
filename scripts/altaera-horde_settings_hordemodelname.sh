4d48cc7407461a86ed8093d518f327d5486775b9
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-06-19 14:26:17 +0200

    Create altaera-horde_settings_hordemodelname.sh

---
+++ scripts/altaera-horde_settings_hordemodelname.sh
@@ -0,0 +1,11 @@
+cat altaera-horde_settings_hordemodelname.sh
+!/bin/bash
+
+# Redirecting dialog box output to a temporary stream
+exec 3>&1  # Save the place that stdout (1) points to
+result=$(dialog --title "Input Required" --inputbox "Enter your Horde Model Name:" 10 30 2>&1 >/dev/tty)
+exec 3>&-  # Close the temporary stream
+
+            cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
+            sed -i '/--hordemodelname/c\--hordemodelname $result \\' altaera.sh
+            cd '/data/data/com.termux/files/home'
