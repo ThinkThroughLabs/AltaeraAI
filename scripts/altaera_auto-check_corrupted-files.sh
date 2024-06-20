57269c7446b6bf035b6d09eab8322a4abd98c673
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-06-20 15:08:28 +0200

    Update altaera_auto-check_corrupted-files.sh

--- scripts/altaera_auto-check_corrupted-files.sh
+++ scripts/altaera_auto-check_corrupted-files.sh
@@ -8,10 +8,81 @@ response=$?
 case $response in
 
         0)      
+            clear
+            echo "
+
+        ██████████
+       ██ █
+      ██  █
+     ██   █
+     █    ████████
+    ██    █
+   ████████
+  ██      █
+  █       ████████
+
+  AltaeraAI - v5.2
+
+    by ThinkThroughLabs
+
+
+  "
+
+  echo "Did you know...
+
+  " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+
+  	function random_message() {
+    # Array of random messages
+    messages=("that KobbleTiny is the world's sweetest child?" "that KobbleTiny is concedo's designed mind?")
+
+    # Generate a random index
+    index=$(( RANDOM % ${#messages[@]} ))
+
+    # Get the random message
+    message=${messages[$index]}
+
+    # Print the random message
+    echo $message | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+}
+
+# Call the function
+random_message
+
+echo "________________________________________________________________
+" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+
+            echo "Initializing fix...
+
+            " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+            {
         rm -rf 'altaera_auto-check_fix.sh'
         wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera_auto-check_fix.sh
+        chmod a+x 'altaera_auto-check_fix.sh'
+            } &> /dev/null 2>&1;
+            clear
+            echo "Initializing fix ✔
+
+            " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+            
+            echo "Fixing AltaeraAI essential files..."
+
+            " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+            {
         bash 'altaera_auto-check_fix.sh'
         rm -rf 'altaera_auto-check_fix.sh'
+            } &> /dev/null 2>&1;
+            clear
+            echo "Initializing fix ✔
+
+            " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+            
+            echo "Fixing AltaeraAI essential files ✔"
+
+            " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+            
+sleep .5
+
           ;;
         1) 
         exit
