8f386e45c5cbff3d6590ff84d13a47e6840890d2
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-06-20 13:29:32 +0200

    Update altaera_auto-check.sh

--- scripts/altaera_auto-check.sh
+++ scripts/altaera_auto-check.sh
@@ -39,11 +39,16 @@ sleep .5
 if [ -f /AltaeraAI/altaera.sh ] || [ -f /AltaeraAI/altaeralogin.sh ] || [ -f /AltaeraAI/altaera-update.sh ] || [ -f /AltaeraAI/altaera-uninstall.sh ] || [ -f /AltaeraAI/altaera-reinstall.sh ] || [ -f /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/altaera.sh ] || [ -f /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/benchmark.sh ] || [ -f /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/horde.sh ]
  then
      clear
-             echo "Running essential file integrity checks ✔
+        echo "Automatically checking for updates ✔
+        
+        ";
+
+        echo "Running essential file integrity checks ✔
 
         ";
 
 sleep .5
+
  else
      bash 'AltaeraAI/altaera_auto-check_corrupted-files.sh'
  fi
