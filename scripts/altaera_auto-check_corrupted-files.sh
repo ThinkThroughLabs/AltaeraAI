375b1228ffa973f566b12c231033b8c48464ae22
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-06-20 12:45:33 +0200

    Create altaera_auto-check_corrupted-files.sh

---
+++ scripts/altaera_auto-check_corrupted-files.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+dialog --title "Corrupted Files" \
+        --backtitle "AltaeraAI - Launch" \
+        --yesno "Some of the essential files are missing. Would you like to install them now?" 7 60
+
+response=$?
+case $response in
+
+        0)      
+        rm -rf 'altaera_auto-check_fix.sh'
+        wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera_auto-check_fix.sh
+        bash 'altaera_auto-check_fix.sh'
+        rm -rf 'altaera_auto-check_fix.sh'
+          ;;
+        1) 
+        exit
+        ./data/data/com.termux/files/home/AltaeraAI/altaera_no-check.sh;;
+        255)
+        exit
+        ./data/data/com.termux/files/home/AltaeraAI/altaera_no-check.sh;;
+esac
