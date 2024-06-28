07e0c477872f3e04d743c27e4b265561ef9d5120
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-06-28 19:00:09 +0200

    Update altaera-changelog_cli.sh

--- scripts/altaera-changelog_cli.sh
+++ scripts/altaera-changelog_cli.sh
@@ -3,7 +3,11 @@
 dialog --backtitle "AltaeraAI" \
 --title "AltaeraAI - Changelog [CLI]" \
 --msgbox '
-    * v5.2.3 - introduced "File integrity checks", which run simultaneously with the update checking mechanism, to determine potential file deficiences that can impact AltaeraAIs functionality; in case there are missing files, the user will be asked whether to carry out file repair. Said functionality is set by default (as are automatic update checks), but can also be disabled under settings
+    * v5.2.3 - fixed a bug in the file integrity checking mechanism that reported missing files even when they were not missing (especially after a fresh installation)
+    * added an initial solution to when the PRoot Distro environment fails to install, during the installation process
+    * v5.2.2 - added the ability to enter a custom value for Context & Blas Batch Size, in addition to the fixed sizes
+    * various visual improvements and fixes to the MENU
+    * v5.2.1 - introduced "File integrity checks", which run simultaneously with the update checking mechanism, to determine potential file deficiences that can impact AltaeraAIs functionality; in case there are missing files, the user will be asked whether to carry out file repair. Said functionality is set by default (as are automatic update checks), but can also be disabled under settings
     * visual fixes and improvements, changes to the MENU
     * v5.2 – updated to koboldcpp-1.68
     * added the “Horde” option to the MENU, which utilises AI Horde to allow for sharing your processing power (an AI Model) for users worldwide
