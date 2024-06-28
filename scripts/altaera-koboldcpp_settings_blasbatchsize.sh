1490511471ce2f85c7c0b33f51c47e8b13868372
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-06-28 12:41:54 +0200

    Update altaera-koboldcpp_settings_blasbatchsize.sh

--- scripts/altaera-koboldcpp_settings_blasbatchsize.sh
+++ scripts/altaera-koboldcpp_settings_blasbatchsize.sh
@@ -7,16 +7,17 @@ BACKTITLE="AltaeraAI - KoboldCpp Settings"
 TITLE="Blas Batch Size"
 MENU="Choose the blas batch size:"
 
-OPTIONS=(1 "[...] Go Back"
-         2 "2048 [Default]"
-	 3 "-1 [Good for low RAM]"
-         4 "1"
-         5 "32"
-         6 "64"
-         7 "128"
-         8 "256"
-         9 "512"
-         10 "1024")
+OPTIONS=(1 "[...] Go back"
+         2 "Enter value"
+         3 "2048 [Default]"
+	 4 "-1 [Good for low RAM]"
+         5 "1"
+         6 "32"
+         7 "64"
+         8 "128"
+         9 "256"
+         10 "512"
+         11 "1024")
 
 
 CHOICE=$(dialog --clear \
@@ -33,54 +34,68 @@ case $CHOICE in
             exit
 	    ;;
         2)
+            cat altaera-koboldcpp_settings_blasbatchsize.sh
+            !/bin/bash
+
+            # Redirecting dialog box output to a temporary stream
+            exec 3>&1  # Save the place that stdout (1) points to
+            result=$(dialog --title "Input Required" --inputbox "Enter a Blas Batch Size value:" 10 30 2>&1 >/dev/tty)
+            exec 3>&-  # Close the temporary stream
+
+            cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
+            sed -i '/--blasbatchsize/c\--blasbatchsize '$result' \\' altaera.sh
+            cd '/data/data/com.termux/files/home'
+	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Blas Batch Size was successfully changed to "'$result'"!' 10 30
+            ;;
+        3)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--blasbatchsize/c\--blasbatchsize 2048 \\' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Blas Batch Size was successfully changed to "2048"!' 10 30
             ;;
-        3)
+        4)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--blasbatchsize/c\--blasbatchsize -1 \\' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Blas Batch Size was successfully changed to "-1"!' 10 30
             ;;
-        4)
+        5)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--blasbatchsize/c\--blasbatchsize 1 \\' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Blas Batch Size was successfully changed to "1"!' 10 30
             ;;
-        5)
+        6)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--blasbatchsize/c\--blasbatchsize 32 \\' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Blas Batch Size was successfully changed to "32"!' 10 30
             ;;
-        6)
+        7)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--blasbatchsize/c\--blasbatchsize 64 \\' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Blas Batch Size was successfully changed to "64"!' 10 30
             ;;
-        7)
+        8)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--blasbatchsize/c\--blasbatchsize 128 \\' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Blas Batch Size was successfully changed to "128"!' 10 30
             ;;
-        8)
+        9)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--blasbatchsize/c\--blasbatchsize 256 \\' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Blas Batch Size was successfully changed to "256"!' 10 30
             ;;
-        9)
+        10)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--blasbatchsize/c\--blasbatchsize 512 \\' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Blas Batch Size was successfully changed to "512"!' 10 30
             ;;
-        10)
+        11)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--blasbatchsize/c\--blasbatchsize 1024 \\' altaera.sh
             cd '/data/data/com.termux/files/home'
