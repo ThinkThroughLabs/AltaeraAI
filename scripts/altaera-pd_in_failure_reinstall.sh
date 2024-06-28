38192a1011fa86f1e490e61be40e90de4b4d8a61
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-06-28 19:55:48 +0200

    Update altaera-pd_in_failure_reinstall.sh

--- scripts/altaera-pd_in_failure_reinstall.sh
+++ scripts/altaera-pd_in_failure_reinstall.sh
@@ -24,17 +24,15 @@ echo "The PRoot Distro environment failed to install. Retrying in 5 seconds...
 
 [to cancel, press Ctrl+C]
 "
-echo "10..."
-sleep 1
-echo "9..."
-sleep 1
-echo "8..."
-sleep 1
-echo "7..."
-sleep 1
-echo "6..."
-sleep 1
 echo "5..."
+sleep 1
+echo "4..."
+sleep 1
+echo "3..."
+sleep 1
+echo "2..."
+sleep 1
+echo "1..."
 sleep 1
 
 clear
