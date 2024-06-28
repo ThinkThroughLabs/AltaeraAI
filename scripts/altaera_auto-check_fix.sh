cdc9bde260fe23ed5cd2ae8ec003964ed772a578
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-06-28 13:05:06 +0200

    Update altaera_auto-check_fix.sh

--- scripts/altaera_auto-check_fix.sh
+++ scripts/altaera_auto-check_fix.sh
@@ -15,12 +15,6 @@ chmod a+x 'altaera-horde.sh'
 rm -rf 'altaeralogin.sh'
 wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaeralogin.sh
 chmod a+x 'altaeralogin.sh'
-rm -rf 'altaera_auto-check.sh'
-wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera_auto-check.sh
-chmod a+x 'altaera_auto-check.sh'
-rm -rf 'altaera_auto-check_corrupted-files.sh'
-wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera_auto-check_corrupted-files.sh
-chmod a+x 'altaera_auto-check_corrupted-files.sh'
 rm -rf 'altaera_no-check.sh'
 wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera_no-check.sh
 chmod a+x 'altaera_no-check.sh'
