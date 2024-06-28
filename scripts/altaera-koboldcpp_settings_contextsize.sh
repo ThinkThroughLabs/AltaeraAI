feeef05ae5ede57553b178c1ac4f9af44ca9f166
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-06-28 12:33:13 +0200

    Update altaera-koboldcpp_settings_contextsize.sh

--- scripts/altaera-koboldcpp_settings_contextsize.sh
+++ scripts/altaera-koboldcpp_settings_contextsize.sh
@@ -7,19 +7,20 @@ BACKTITLE="AltaeraAI - KoboldCpp Settings"
 TITLE="Context Size"
 MENU="Choose the context size:"
 
-OPTIONS=(1 "[...] Go Back"
-         2 "2048 [Default]"
-         3 "512"
-         4 "1024"
-         5 "3072"
-         6 "4096"
-         7 "6144"
-         8 "8192"
-         9 "12288"
-         10 "16384"
-         11 "24576"
-         12 "32768"
-         13 "65536")
+OPTIONS=(1 "[...] Go back"
+         2 "Enter value"
+         3 "2048 [Default]"
+         4 "512"
+         5 "1024"
+         6 "3072"
+         7 "4096"
+         8 "6144"
+         9 "8192"
+         10 "12288"
+         11 "16384"
+         12 "24576"
+         13 "32768"
+         14 "65536")
 
 
 CHOICE=$(dialog --clear \
@@ -36,72 +37,85 @@ case $CHOICE in
             exit
 	    ;;
         2)
+            cat altaera-koboldcpp_settings_contextsize.sh
+            !/bin/bash
+
+            # Redirecting dialog box output to a temporary stream
+            exec 3>&1  # Save the place that stdout (1) points to
+            result=$(dialog --title "Input Required" --inputbox "Enter custom URL for a HuggingFace model (GGUF):" 10 30 2>&1 >/dev/tty)
+            exec 3>&-  # Close the temporary stream
+
+            cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
+            sed -i '/--contextsize/c\--contextsize '$result'' altaera.sh
+            cd '/data/data/com.termux/files/home'
+	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Context Size was successfully changed to "'$result'"!' 10 30
+        3)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--contextsize/c\--contextsize 2048' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Context Size was successfully changed to "2048"!' 10 30
             ;;
-        3)
+        4)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--contextsize/c\--contextsize 512' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Context Size was successfully changed to "512"!' 10 30
             ;;
-        4)
+        5)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--contextsize/c\--contextsize 1024' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Context Size was successfully changed to "1024"!' 10 30
             ;;
-        5)
+        6)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--contextsize/c\--contextsize 3072' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Context Size was successfully changed to "3072"!' 10 30
             ;;
-        6)
+        7)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--contextsize/c\--contextsize 4096' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Context Size was successfully changed to "4096"!' 10 30
             ;;
-        7)
+        8)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--contextsize/c\--contextsize 6144' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Context Size was successfully changed to "6144"!' 10 30
             ;;
-        8)
+        9)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--contextsize/c\--contextsize 8192' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Context Size was successfully changed to "8192"!' 10 30
             ;;
-        9)
+        10)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--contextsize/c\--contextsize 12288' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Context Size was successfully changed to "12288"!' 10 30
             ;;
-        10)
+        11)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--contextsize/c\--contextsize 16384' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Context Size was successfully changed to "16384"!' 10 30
             ;;
-        11)
+        12)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--contextsize/c\--contextsize 24576' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Context Size was successfully changed to "24576"!' 10 30
             ;;
-        12)
+        13)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--contextsize/c\--contextsize 32768' altaera.sh
             cd '/data/data/com.termux/files/home'
 	    dialog --title "AltaeraAI - KoboldCpp Settings" --msgbox 'Context Size was successfully changed to "32768"!' 10 30
             ;;
-        13)
+        14)
             cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root'
             sed -i '/--contextsize/c\--contextsize 65536' altaera.sh
             cd '/data/data/com.termux/files/home'
