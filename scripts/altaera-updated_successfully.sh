d89081d865f9ca90343ea1da9dd5cb618461d8fc
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-07-06 17:23:43 +0200

    Update altaera-updated_successfully.sh

--- scripts/altaera-updated_successfully.sh
+++ scripts/altaera-updated_successfully.sh
@@ -7,16 +7,15 @@ dialog --backtitle "AltaeraAI" \
 
 Recent changes:
 
-    * v5.4 - updated to koboldcpp-1.69
+    * v5.4 - updated to koboldcpp-1.69.1
+    * added Gemma-2-9B-it weights to the list of models
+    * v5.3 - updated to koboldcpp-1.69
     * v5.2.3 - fixed a bug in the file integrity checking mechanism that reported missing files even when they were not (especially after a fresh installation)
     * added an initial solution to when the PRoot Distro environment fails to install, during the installation process
     * v5.2.2 - added the ability to enter a custom value for Context & Blas Batch Size, in addition to the fixed sizes
     * various visual improvements and fixes to the MENU
     * v5.2.1 - introduced "File integrity checks", which run simultaneously with the update checking mechanism, to determine potential file deficiences that can impact AltaeraAIs functionality; in case there are missing files, the user will be asked whether to carry out file repair. Said functionality is set by default (as are automatic update checks), but can also be disabled under settings
     * visual fixes and improvements, changes to the MENU
-    * v5.2 – updated to koboldcpp-1.68
-    * added the “Horde” option to the MENU, which utilises AI Horde to allow for sharing your processing power (an AI Model) for users worldwide
-    * minor aesthetic changes and fixes to the MENU
 ' 25 40
 
 bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
