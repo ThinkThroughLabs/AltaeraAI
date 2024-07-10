9b59d2a0ab83b39707c744d62054e36cad7b9188
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-07-10 13:45:56 +0200

    Update altaera_update-termux.sh

--- scripts/altaera_update-termux.sh
+++ scripts/altaera_update-termux.sh
@@ -46,9 +46,9 @@ echo "________________________________________________________________
 
             " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
           {
-                  termux-setup-storage
                   cd '/sdcard/Download'
                   wget https://github.com/ThinkThroughLabs/termux-releases/releases/download/0.118.1/com.termux_118.1.apk
+                  chmod a+x 'com.termux_118.1.apk'
           } &> /dev/null 2>&1;
           
             clear
