23bca62dd24cabbfc4a61c4f09c0bd54ed73f982
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-07-10 12:01:53 +0200

    Create altaera_update-termux.sh

---
+++ scripts/altaera_update-termux.sh
@@ -0,0 +1,6 @@
+         {
+         termux-setup-storage
+         cd '/sdcard/Download'
+         wget https://github.com/ThinkThroughLabs/termux-releases/releases/download/0.118.1/com.termux_118.1.apk
+         termux-open 'com.termux_118.1.apk'
+         } &> /dev/null 2>&1;
