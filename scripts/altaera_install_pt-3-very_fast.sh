849091e5ef3e4f031a5ea1c2dbd8f867e0e32ee2
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-07-12 14:54:16 +0200

    Update altaera_install_pt-3-very_fast.sh

--- scripts/altaera_install_pt-3-very_fast.sh
+++ scripts/altaera_install_pt-3-very_fast.sh
@@ -175,11 +175,11 @@ echo "Extracting pre-packaged PRoot-Distro ✔
 
 " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
 
-echo "Installing core files...
+echo "Installing internal Termux files...
 
 " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
 
-cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera'
+cd '/data/data/com.termux/files/home'
 {
 rm -rf "/etc/bash.bashrc"
 wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/bash.bashrc
@@ -188,52 +188,6 @@ rm -rf 'bash.bashrc'
 dialog --create-rc ~/.dialogrc
 sed -i '/^screen_color = (CYAN,BLUE,ON)/d' ~/.dialogrc
 echo "screen_color = (CYAN,BLACK,ON)" >> ~/.dialogrc
-wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/altaera.sh
-chmod a+x 'altaera.sh'
-wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/altaera_pre-launch.sh
-chmod a+x 'altaera_pre-launch.sh'
-wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/horde.sh
-chmod a+x 'horde.sh'
-wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/altaera-fix.sh
-chmod a+x 'altaera-fix.sh'
-wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/altaera-fixed_successfully.sh
-chmod a+x 'altaera-fixed_successfully.sh'
-wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/altaera_corrupted-files.sh
-chmod a+x 'altaera_corrupted-files.sh'
-wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/benchmark.sh
-chmod a+x 'benchmark.sh'
-wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/altaera-model_empty.sh
-chmod a+x 'altaera-model_empty.sh'
-wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/altaera-model_list_empty.sh
-chmod a+x 'altaera-model_list_empty.sh'
-wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/altaera-model_list.sh
-chmod a+x 'altaera-model_list.sh'
-wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/altaera-model_remove_in.sh
-chmod a+x 'altaera-model_remove_in.sh'
-wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/altaera-model_backup.sh
-chmod a+x 'altaera-model_backup.sh'
-wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/dialog_theme-on.sh
-chmod a+x 'dialog_theme-on.sh'
-wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/dialog_theme-off.sh
-chmod a+x 'dialog_theme-off.sh'
-wget https://github.com/ThinkThroughLabs/AltaeraAI/releases/download/v5.4/altaera-v5.4.tar.gz
-tar -xf 'altaera-v5.4.tar.gz'
-rm -rf 'altaera-v5.4.tar.gz'
-      rm -rf 'koboldcpp-altaera'
-      rm -rf 'kcpp-ae_cm'
-      wget https://github.com/ThinkThroughLabs/AltaeraAI/releases/download/v3.1/altaera-v3.1.tar.gz
-      tar -xf 'altaera-v3.1.tar.gz'
-      mv 'koboldcpp-altaera' 'kcpp-ae_cm'
-      rm -rf 'altaera-v3.1.tar.gz'
-      cd 'kcpp-ae_cm'
-      rm -rf 'klite.embd'
-      wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/klite/klite.embd
-      cd ..
-cd 'kcpp-ae'
-rm -rf 'models'
-rm -rf 'klite.embd'
-wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/klite/klite.embd
-cd '/root'
 wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera_install_pt-4.sh
 chmod a+x 'altaera_install_pt-4.sh'
 } &> /dev/null 2>&1;
