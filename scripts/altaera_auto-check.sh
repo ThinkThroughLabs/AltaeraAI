3ae0d9ebc32345c0337b680f9c1536a3a9b652c4
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-07-22 16:06:56 +0200

    Update altaera_auto-check.sh

--- scripts/altaera_auto-check.sh
+++ scripts/altaera_auto-check.sh
@@ -15,7 +15,7 @@ clear
           else
           dialog --backtitle "AltaeraAI" \
           --title "AltaeraAI - Technical Difficulties" \
-          --msgbox 'AltaeraAI is experiencing malfunctions at this very moment. If you have recently downloaded or updated it, chances are some things will be broken. Please, give us some time to carry out code repairs. Sorry for the inconvenience!' 20 40
+          --msgbox 'AltaeraAI is experiencing malfunctions at this very moment. If you have recently downloaded or updated it, chances are some things will be broken. Please, give us some time to carry out code repairs. Sorry for the inconvenience!' 30 40
 
         fi
 
