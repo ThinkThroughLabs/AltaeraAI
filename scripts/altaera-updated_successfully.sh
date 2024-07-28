405a3acca187887ad47d3aefed6cf53851830a6d
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-07-28 14:35:06 +0200

    Update altaera-updated_successfully.sh

--- scripts/altaera-updated_successfully.sh
+++ scripts/altaera-updated_successfully.sh
@@ -15,7 +15,6 @@ Recent changes:
     * v5.4 - updated to koboldcpp-1.69.1
     * added Gemma-2-9B-it weights to the list of models
     * v5.3 - updated to koboldcpp-1.69
-    * v5.2.3 - fixed a bug in the file integrity checking mechanism that reported missing files even when they were not (especially after a fresh installation)
 ' 25 40
 
 bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
