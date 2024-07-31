b223cc2d740f88ef1895ff8a00b1a50d41519d67
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-07-31 13:14:00 +0200

    Update altaera_install_pt-4.sh

--- scripts/altaera_install_pt-4.sh
+++ scripts/altaera_install_pt-4.sh
@@ -84,6 +84,30 @@ rm -rf 'altaera_install-architecture.sh'
 
 clear
 
+if [ -f data/data/com.termux/files/home/AltaeraAI/very_fast-tmp.sh ]
+then
+echo "**********INSTALLATION COMPLETED**********
+" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+echo "________________________________________________________________
+" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+echo "
+      !!!  - Please, pull down the notification bar, expand 'Termux' and press 'Exit'  !!!
+      !!!  - Or, press 'CTRL+D' twice.                                                !!!
+
+      --- Then, open Termux and:
+      
+- To start, please type in 'ae'.
+
+            [Please, ignore the 'ERROR' below if it shows up]"
+echo "________________________________________________________________
+" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+
+rm 'data/data/com.termux/files/home/AltaeraAI/very_fast-tmp.sh'
+exec <&-
+else
+:
+fi
+
 echo "**********INSTALLATION COMPLETED**********
 " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
 echo "________________________________________________________________
