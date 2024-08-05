97b83c6c639c91f6ef8c8499bbf6895ea6213756
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-08-05 12:57:14 +0200

    Update altaera-changelog_cli.sh

--- scripts/altaera-changelog_cli.sh
+++ scripts/altaera-changelog_cli.sh
@@ -3,7 +3,10 @@
 dialog --backtitle "AltaeraAI" \
 --title "AltaeraAI - Changelog [CLI]" \
 --msgbox '
-    * v5.7 - updated to koboldcpp-1.71.1
+    * v5.7.1 - added Gemma-2-2B-it and Gemma-2-2B-it-abliterated weights to the list of models
+    * v5.7 - updated to koboldcpp-1.72
+    * re-written the installation script to be more automated and user friendly, added visual enhancements and fixed bugs related to it
+    * v5.6 - updated to koboldcpp-1.71.1
     * v5.5.2 - fixed a bug that caused the “Functional Status” check to always inform the user of technical difficulties
     * v5.5.1 - a “Functional Status” check has been added, which will notify the user of ongoing technical difficulties (as determined by the repository owner) that may occur for users who have recently installed or upgraded AltaerAI, due to its rolling-release lifecycle
     * v5.5 - updated to koboldcpp-1.70.1
