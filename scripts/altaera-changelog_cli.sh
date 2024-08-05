32eaf5f962b1e20ba651eeb73954e5ce756d19d7
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-08-05 13:04:37 +0200

    Update altaera-changelog_cli.sh

--- scripts/altaera-changelog_cli.sh
+++ scripts/altaera-changelog_cli.sh
@@ -8,7 +8,7 @@ dialog --backtitle "AltaeraAI" \
     * re-written the installation script to be more automated and user friendly, added visual enhancements and fixed bugs related to it
     * v5.6 - updated to koboldcpp-1.71.1
     * v5.5.2 - fixed a bug that caused the “Functional Status” check to always inform the user of technical difficulties
-    * v5.5.1 - a “Functional Status” check has been added, which will notify the user of ongoing technical difficulties (as determined by the repository owner) that may occur for users who have recently installed or upgraded AltaerAI, due to its rolling-release lifecycle
+    * v5.5.1 - a “Functional Status” check has been added, which will notify the user of ongoing technical difficulties (as determined by the repository owner) that may occur for users who have recently installed or upgraded AltaeraAI, due to its rolling-release lifecycle
     * v5.5 - updated to koboldcpp-1.70.1
     * v5.4.2 - a "very fast" installation method has been introduced that drastically shortens the process. It uses a pre-packaged PRoot-Distro environment, instead of installing it from scratch, and restores it in a backup form. It is now a default installation method
     * v5.4.1 - added an automatic Termux update check, which will inform the user of an available update and allow the user to choose whether Termux was initially downloaded from GitHub or from F-Droid to further proceed (this is necessary for the update to actually work). This functionality is set by default (as are automatic update & file integrity checks), but can also be disabled under settings
