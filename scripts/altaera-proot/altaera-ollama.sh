dfe6d6c0d9bf068a7320608767d9e3e8349c046d
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-08-13 17:08:27 +0200

    Update altaera-ollama.sh

--- scripts/altaera-proot/altaera-ollama.sh
+++ scripts/altaera-proot/altaera-ollama.sh
@@ -1,2 +1,36 @@
-ollama serve
-curl -fsSL https://ollama.com/install.sh | sh
+#!/bin/bash
+
+HEIGHT=25
+WIDTH=160
+CHOICE_HEIGHT=24
+BACKTITLE="AltaeraAI - v5.7.1 | RAM:$(free -m | awk '/^Mem:/{printf("%.1fG\n",$2/1000)}') | Free Storage:$(df -Ph . | tail -1 | awk '{print $4}')"
+TITLE="AltaeraAI - ollama"
+MENU="Choose what to do:"
+
+OPTIONS=(1 "[...] Go Back"
+         2 "Initialize ollama [do this first!]"
+         3 "Start ollama")
+
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+
+        1)
+            exit
+            ./altaera.sh
+            ;;
+        2)
+            ollama serve
+            ;;
+        3)
+            ;;
+
+esac
