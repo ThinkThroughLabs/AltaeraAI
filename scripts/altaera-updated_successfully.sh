bfd8892dd57c852b25dda444865f1d05d05bded8
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-08-16 10:39:00 +0200

    Update altaera-updated_successfully.sh

--- scripts/altaera-updated_successfully.sh
+++ scripts/altaera-updated_successfully.sh
@@ -7,12 +7,12 @@ dialog --backtitle "AltaeraAI" \
 
 Recent changes:
 
-    * v5.7.2 - added Gemma-2-2B-it and Gemma-2-2B-it-abliterated weights to the list of models
+    * v5.7.2 – added initial ollama support
+    * v5.7.1 - added Gemma-2-2B-it and Gemma-2-2B-it-abliterated weights to the list of models
     * v5.7 - updated to koboldcpp-1.72
     * re-written the installation script to be more automated and user friendly, added visual enhancements and fixed bugs related to it
     * v5.6 - updated to koboldcpp-1.71.1
     * v5.5.2 - fixed a bug that caused the “Functional Status” check to always inform the user of technical difficulties
-    * v5.5.1 - a “Functional Status” check has been added, which will notify the user of ongoing technical difficulties (as determined by the repository owner) that may occur for users who have recently installed or upgraded AltaerAI, due to its rolling-release lifecycle
 ' 25 40
 
 bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
