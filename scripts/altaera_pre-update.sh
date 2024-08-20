9216f222652a6948e3a4a724d205a408686b828d
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-08-20 11:38:16 +0200

    Update altaera_pre-update.sh

--- scripts/altaera_pre-update.sh
+++ scripts/altaera_pre-update.sh
@@ -2,7 +2,7 @@
 
         echo "Establishing whether a package update is needed...
         ";
-        
+         if [ -d /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/kcpp-ae ]; then
                 {
                 cd "AltaeraAI"
                 rm -rf 'altaera-version_package.sh'
@@ -73,4 +73,62 @@ echo "________________________________________________________________
           exit
           else
           bash 'AltaeraAI/altaera-update.sh'
+          
+          elif [ ! -d /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/kcpp-ae ]; then
+            clear
+            echo "
+
+        ██████████
+       ██ █
+      ██  █
+     ██   █
+     █    ████████
+    ██    █
+   ████████
+  ██      █
+  █       ████████
+
+  AltaeraAI - v5.7.2
+
+    by ThinkThroughLabs
+
+
+  "
+
+    echo "Did you know...
+
+  " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+
+  	function random_message() {
+    # Array of random messages
+    messages=("that KobbleTiny is the world's sweetest child?" "that KobbleTiny is concedo's designed mind?")
+
+    # Generate a random index
+    index=$(( RANDOM % ${#messages[@]} ))
+
+    # Get the random message
+    message=${messages[$index]}
+
+    # Print the random message
+    echo $message | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+}
+
+# Call the function
+random_message
+
+echo "________________________________________________________________
+" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+
+            echo "Initializing update...
+
+            " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+          {
+          wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-update_content-scripts.sh -O 'altaera-update_content.sh'
+          chmod a+x 'altaera-update_content.sh'
+          } &> /dev/null 2>&1;
+          bash 'altaera-update_content.sh'
+          clear
+          bash 'altaera-updated_successfully.sh'
+          exit
+         fi
          fi
