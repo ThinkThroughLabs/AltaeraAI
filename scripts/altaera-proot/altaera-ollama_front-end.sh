82600076e7917a37929f17e277be6e6373eb2f86
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-08-20 10:25:01 +0200

    Update altaera-ollama_front-end.sh

--- scripts/altaera-proot/altaera-ollama_front-end.sh
+++ scripts/altaera-proot/altaera-ollama_front-end.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-HEIGHT=25
-WIDTH=160
+HEIGHT=15
+WIDTH=80
 CHOICE_HEIGHT=24
 BACKTITLE="AltaeraAI - v5.7.2 | RAM:$(free -m | awk '/^Mem:/{printf("%.1fG\n",$2/1000)}') | Free Storage:$(df -Ph . | tail -1 | awk '{print $4}')"
 TITLE="AltaeraAI - ollama Official Models: Front-end"
