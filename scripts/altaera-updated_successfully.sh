06b3784d9707518f09ccf53e82a63cc13adc9d41
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-08-21 14:40:38 +0200

    Update altaera-updated_successfully.sh

--- scripts/altaera-updated_successfully.sh
+++ scripts/altaera-updated_successfully.sh
@@ -7,12 +7,15 @@ dialog --backtitle "AltaeraAI" \
 
 Recent changes:
 
-    * v5.7.3 – added initial ollama support
+    * v5.7.3 - updated the pre-packaged KoboldCpp to v1.73
+    * a mechanism has been added so that when updating AltaeraAI, the pre-packaged KoboldCpp will update only if it was previously installed
+    * added ability to use “maid” as an external front-end for ollama
+    * various fixes and expanded ollama support
+    * v5.7.2 – added initial ollama support
     * v5.7.1 - added Gemma-2-2B-it and Gemma-2-2B-it-abliterated weights to the list of models
     * v5.7 - updated to koboldcpp-1.72
     * re-written the installation script to be more automated and user friendly, added visual enhancements and fixed bugs related to it
     * v5.6 - updated to koboldcpp-1.71.1
-    * v5.5.2 - fixed a bug that caused the “Functional Status” check to always inform the user of technical difficulties
 ' 25 40
 
 bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
