af8f76690e688edf8f7050fa194d489258078a38
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-08-21 14:40:12 +0200

    Update altaera-changelog_cli.sh

--- scripts/altaera-changelog_cli.sh
+++ scripts/altaera-changelog_cli.sh
@@ -3,7 +3,11 @@
 dialog --backtitle "AltaeraAI" \
 --title "AltaeraAI - Changelog [CLI]" \
 --msgbox '
-    * v5.7.3 – added initial ollama support
+    * v5.7.3 - updated the pre-packaged KoboldCpp to v1.73
+    * a mechanism has been added so that when updating AltaeraAI, the pre-packaged KoboldCpp will update only if it was previously installed
+    * added ability to use “maid” as an external front-end for ollama
+    * various fixes and expanded ollama support
+    * v5.7.2 – added initial ollama support
     * v5.7.1 - added Gemma-2-2B-it and Gemma-2-2B-it-abliterated weights to the list of models
     * v5.7 - updated to koboldcpp-1.72
     * re-written the installation script to be more automated and user friendly, added visual enhancements and fixed bugs related to it
