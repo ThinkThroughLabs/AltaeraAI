60e5e8ca7e607c5c740f4db3254384f1fac17047
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-08-26 11:42:24 +0200

    Update altaera-ollama_install.sh

--- scripts/altaera-proot/altaera-ollama_install.sh
+++ scripts/altaera-proot/altaera-ollama_install.sh
@@ -1,3 +1,8 @@
 curl -fsSL https://ollama.com/install.sh | sh
 
+clear
+echo "ollama intialized! Now press 'Ctrl+T', type in 'ae' and start ollama by choosing a model"
+
+{
 ollama serve
+} &> /dev/null 2>&1;
