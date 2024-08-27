ccab4c7a9187117154144e876dc2bfdcce70e4bb
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-08-27 11:46:14 +0200

    Update altaera-sillytavern_install.sh

--- scripts/altaera-proot/altaera-sillytavern_install.sh
+++ scripts/altaera-proot/altaera-sillytavern_install.sh
@@ -2,6 +2,9 @@ cd '/root'
 git clone https://github.com/SillyTavern/SillyTavern
 npm install
 
+if [ -d '/root/SillyTavern' ]
+then
+
 dialog --title "SillyTavern - Installation" \
         --backtitle "AltaeraAI - SillyTavern" \
         --yesno "SillyTavern was installed successfully! Would you like to launch it now?" 7 60
@@ -16,4 +19,9 @@ case $response in
         ./data/data/com.termux/files/home/AltaeraAI/altaera.sh;;
         255) exit
         ./data/data/com.termux/files/home/AltaeraAI/altaera.sh;;
+
+else
+
+echo "There were problems installing SillyTavern. You can try to install it again, or wait until proper AltaeraAI fixes are implemented"
+
 esac
