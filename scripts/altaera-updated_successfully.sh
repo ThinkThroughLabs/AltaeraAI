57e69ea09c64a5e17274dea8876873632ce87bf3
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-08-31 12:36:17 +0200

    Update altaera-updated_successfully.sh

--- scripts/altaera-updated_successfully.sh
+++ scripts/altaera-updated_successfully.sh
@@ -7,7 +7,8 @@ dialog --backtitle "AltaeraAI" \
 
 Recent changes:
 
-    * v5.7.5 - Hotfix: fixed code mistakes related to the "Manage AI Back-ends" option
+    * v5.7.5 - updated the pre-packaged KoboldCpp to v1.74
+    * v5.7.4.2 - Hotfix: fixed code mistakes related to the "Manage AI Back-ends" option
     * v5.7.4.1 - Hotfix: fixed the issue related to the new version of pre-packaged KoboldCpp not being recognised as an update
     * v5.7.4 - updated the pre-packaged KoboldCpp to v1.73.1
     * added initial SillyTavern support
@@ -17,7 +18,6 @@ Recent changes:
     * added ability to use “maid” as an external front-end for ollama
     * various fixes and expanded ollama support
     * v5.7.2 – added initial ollama support
-    * v5.7.1 - added Gemma-2-2B-it and Gemma-2-2B-it-abliterated weights to the list of models
 ' 25 40
 
 bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
