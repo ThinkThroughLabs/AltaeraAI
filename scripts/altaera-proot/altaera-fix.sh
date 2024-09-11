ac4f7e4d78fdfbfe96c81363430b17fd43107328
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-09-11 11:40:25 +0200

    Update altaera-fix.sh

--- scripts/altaera-proot/altaera-fix.sh
+++ scripts/altaera-proot/altaera-fix.sh
@@ -5,11 +5,13 @@ WIDTH=100
 CHOICE_HEIGHT=24
 BACKTITLE="AltaeraAI - Fix"
 TITLE="Choosing fixing method"
-MENU="Would you like to fix with the pre-packaged KoboldCpp [Faster], or by building your own? [Slower]"
+MENU="Would you like to install the pre-packaged KoboldCpp [Faster], or compile your own? [Slower]"
 
 OPTIONS=(1 "Fix with pre-packaged KoboldCpp [Faster]"
-         2 "Fix with building your own KoboldCpp [Slower]"
-         3 "Fix with building your own KoboldCpp (No-Blas) [Slower]")
+         2 "Compile own KoboldCpp (No-Blas) [Slower]"
+         3 "Compile own KoboldCpp (Blas) [Slower]"
+         4 "Compile own experimental KoboldCpp (No-Blas) [Slower]"
+         5 "Compile own experimental KoboldCpp (Blas) [Slower]")
 
 
 CHOICE=$(dialog --clear \
@@ -68,7 +70,7 @@ random_message
 echo "________________________________________________________________
 " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
 
-            echo "Initializing fix...
+            echo "Initialazing KoboldCpp installation...
 
             " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
             {
@@ -125,7 +127,7 @@ random_message
 echo "________________________________________________________________
 " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
 
-            echo "Initializing fix...
+            echo "Initialazing KoboldCpp installation...
 
             " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
             {
@@ -182,12 +184,126 @@ random_message
 echo "________________________________________________________________
 " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
 
-            echo "Initializing fix...
+            echo "Initialazing KoboldCpp installation...
 
             " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
             {
             rm -rf 'altaera-fix_core_files.sh'
             wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/altaera-fix_core_files-slow.sh -O 'altaera-fix_core_files.sh'
+            chmod a+x 'altaera-fix_core_files.sh'
+            } &> /dev/null 2>&1;
+            bash 'altaera-fix_core_files.sh'
+            clear
+            bash 'altaera-fixed_successfully.sh'
+            ;;
+        4)
+            clear
+            echo "
+
+        ██████████
+       ██ █
+      ██  █
+     ██   █
+     █    ████████
+    ██    █
+   ████████
+  ██      █
+  █       ████████
+
+  AltaeraAI - v5.7.5
+
+    by ThinkThrough
+
+
+  "
+
+    echo "Did you know...
+
+  " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+
+  	function random_message() {
+    # Array of random messages
+    messages=("that KobbleTiny is the world's sweetest child?" "that KobbleTiny is concedo's designed mind?")
+
+    # Generate a random index
+    index=$(( RANDOM % ${#messages[@]} ))
+
+    # Get the random message
+    message=${messages[$index]}
+
+    # Print the random message
+    echo $message | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+}
+
+# Call the function
+random_message
+
+echo "________________________________________________________________
+" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+
+            echo "Initialazing KoboldCpp installation...
+
+            " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+            {
+            rm -rf 'altaera-fix_core_files.sh'
+            wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/altaera-fix_core_files-slow_no-blas_experimental.sh -O 'altaera-fix_core_files.sh'
+            chmod a+x 'altaera-fix_core_files.sh'
+            } &> /dev/null 2>&1;
+            bash 'altaera-fix_core_files.sh'
+            clear
+            bash 'altaera-fixed_successfully.sh'
+            ;;
+        5)
+            clear
+            echo "
+
+        ██████████
+       ██ █
+      ██  █
+     ██   █
+     █    ████████
+    ██    █
+   ████████
+  ██      █
+  █       ████████
+
+  AltaeraAI - v5.7.5
+
+    by ThinkThrough
+
+
+  "
+
+    echo "Did you know...
+
+  " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+
+  	function random_message() {
+    # Array of random messages
+    messages=("that KobbleTiny is the world's sweetest child?" "that KobbleTiny is concedo's designed mind?")
+
+    # Generate a random index
+    index=$(( RANDOM % ${#messages[@]} ))
+
+    # Get the random message
+    message=${messages[$index]}
+
+    # Print the random message
+    echo $message | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+}
+
+# Call the function
+random_message
+
+echo "________________________________________________________________
+" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+
+            echo "Initialazing KoboldCpp installation...
+
+            " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+            {
+            rm -rf 'altaera-fix_core_files.sh'
+            wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-proot/altaera-fix_core_files-slow_experimental.sh -O 'altaera-fix_core_files.sh'
             chmod a+x 'altaera-fix_core_files.sh'
             } &> /dev/null 2>&1;
             bash 'altaera-fix_core_files.sh'
