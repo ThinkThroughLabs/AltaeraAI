d80494c78e31054d65aa1c3a48f3eb26f3982983
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-09-17 12:18:36 +0200

    Create altaera-c&q_gguf_models.sh

---
+++ scripts/altaera-c&q_gguf_models.sh
@@ -0,0 +1,28 @@
+HEIGHT=20
+WIDTH=60
+CHOICE_HEIGHT=24
+BACKTITLE="AltaeraAI - Converting & Quantizing"
+TITLE="Convert & Quantize GGUF Models"
+MENU="What would you like to do?:"
+
+OPTIONS=(1 "[...] Go Back"
+         2 "Convert HF to GGUF [...]")
+
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            exit
+            ;;
+        2)
+            #####
+            ;;
+esac
