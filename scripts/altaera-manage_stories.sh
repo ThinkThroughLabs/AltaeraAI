b7b68eaee235c4fdec8ad3c916ce89ee4029fa93
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-09-17 12:10:09 +0200

    Create altaera-manage_stories.sh

---
+++ scripts/altaera-manage_stories.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+HEIGHT=20
+WIDTH=60
+CHOICE_HEIGHT=24
+BACKTITLE="AltaeraAI - Stories Management"
+TITLE="Stories' Settings"
+MENU="What would you like to do?:"
+
+OPTIONS=(1 "[...] Go Back"
+         2 "MEGA")
+
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            exit
+            ;;
+        2)
+            clear
+            {
+            pkg in megacmd
+            mega-cmd
+            } &> /dev/null 2>&1;
+            exit
+            ;;
+esac
