132377ea47d50175aefac3ff92806c0af5491940
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-09-18 10:49:50 +0200

    Create altaera-mega.sh

---
+++ scripts/altaera-proot/altaera-mega.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+HEIGHT=20
+WIDTH=60
+CHOICE_HEIGHT=24
+BACKTITLE="AltaeraAI - MEGA Backup/Restore"
+TITLE="Stories' Settings"
+MENU="What would you like to do?:"
+
+OPTIONS=(1 "[...] Go Back"
+         2 "Back-up stories"
+         3 "Restore stories")
+
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            exit
+            ;;
+        2)
+            {
+            mega-cmd
+            } &> /dev/null 2>&1;
+            bash 'altaera-mega_login.sh'
+            mkdir 'AltaeraAI-stories'
+            bash 'altaera-mega_backup.sh'
+            exit
+            ;;
+        3)
+            {
+            mega-cmd
+            } &> /dev/null 2>&1;
+            bash 'altaera-mega_login.sh'
+            mkdir 'AltaeraAI-stories'
+            bash 'altaera-mega_restore.sh'
+            exit
+            ;;
+esac
