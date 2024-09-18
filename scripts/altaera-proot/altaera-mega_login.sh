0118e06b9a81e938a621ab7dbeecf260a2fe0ed6
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-09-18 10:55:25 +0200

    Create altaera-mega_login.sh

---
+++ scripts/altaera-proot/altaera-mega_login.sh
@@ -0,0 +1,12 @@
+cat altaera-mega_login.sh
+!/bin/bash
+
+# Redirecting dialog box output to a temporary stream
+exec 3>&1  # Save the place that stdout (1) points to
+email=$(dialog --title "Input Required" --inputbox "Enter e-mail:" 10 30 2>&1 >/dev/tty)
+exec 3>&-  # Close the temporary stream
+exec 3>&1  # Save the place that stdout (1) points to
+password=$(dialog --title "Input Required" --inputbox "Enter password:" 10 30 2>&1 >/dev/tty)
+exec 3>&-  # Close the temporary stream
+
+            login $email $password
