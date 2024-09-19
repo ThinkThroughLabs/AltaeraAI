9cee3d90b46ce246037be739c6bcb4382e68084f
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-09-19 12:38:25 +0200

    Update altaera-convert_hf_to_gguf.sh

--- scripts/altaera-proot/altaera-convert_hf_to_gguf.sh
+++ scripts/altaera-proot/altaera-convert_hf_to_gguf.sh
@@ -1,5 +1,11 @@
+echo "Preparing files...
+
+  " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+
+{
 mkdir 'altaera-c&q'
 cd 'altaera-c&q'
 git clone https://github.com/ggerganov/llama.cpp
 git pull
-bash 'altaera-convert_url.sh'
+} &> /dev/null 2>&1;
+bash '/root/altaera-convert_url.sh'
