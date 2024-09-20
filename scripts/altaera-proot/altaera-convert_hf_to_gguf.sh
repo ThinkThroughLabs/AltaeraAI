c518485b9afa073a8b65e0301a328c0c05ead0a7
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-09-20 11:26:50 +0200

    Update altaera-convert_hf_to_gguf.sh

--- scripts/altaera-proot/altaera-convert_hf_to_gguf.sh
+++ scripts/altaera-proot/altaera-convert_hf_to_gguf.sh
@@ -3,6 +3,9 @@ echo "Preparing files...
   " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
 
 {
+pacman -S python-pipx python-numpy --noconfirm
+pipx install torch
+pipx ensure path
 mkdir 'altaera-c&q'
 cd 'altaera-c&q'
 git clone https://github.com/ggerganov/llama.cpp
