b9aacf2a9c838a3ef35db4151de4c73b47d0c665
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-09-20 11:36:52 +0200

    Update altaera-convert_url.sh

--- scripts/altaera-proot/altaera-convert_url.sh
+++ scripts/altaera-proot/altaera-convert_url.sh
@@ -6,8 +6,7 @@ exec 3>&1  # Save the place that stdout (1) points to
 result=$(dialog --title "Input Required" --inputbox "Enter repostiory URL for a HuggingFace model (GGUF):" 10 30 2>&1 >/dev/tty)
 exec 3>&-  # Close the temporary stream
 
-            git clone $result
-            cd '$_'
+            git clone $result && cd "!$:t:r"
             rm 'pytorch_model.bin'
             wget $result/resolve/main/pytorch_model.bin
             python '/root/altaera-c&q/llama.cpp/convert_hf_to_gguf.py' './'
