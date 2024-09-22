3c590929ca02bcb064c31c05e5c2245a94ed4821
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-09-22 14:01:38 +0200

    Update altaera-updated_successfully.sh

--- scripts/altaera-updated_successfully.sh
+++ scripts/altaera-updated_successfully.sh
@@ -11,7 +11,7 @@ Recent changes:
     * added option to compile experimental KoboldCpp
     * removed OpenBLAS, which was replaced by the "llamafile" library - it is now set by default
     * v5.7.6 - updated the pre-packaged KoboldCpp to v1.75.2
-    * added the ability to back up stories to the end-to-end encrypted MEGA cloud
+    * added the ability to back up stories to the end-to-end encrypted MEGA cloud service
     * v5.7.5 - updated the pre-packaged KoboldCpp to v1.74
     * v5.7.4.2 - Hotfix: fixed code mistakes related to the "Manage AI Back-ends" option
     * v5.7.4.1 - Hotfix: fixed the issue related to the new version of pre-packaged KoboldCpp not being recognised as an update
