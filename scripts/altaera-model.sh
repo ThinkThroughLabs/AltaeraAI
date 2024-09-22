4c6a494c362ae912f72f3ddb61f240d9733710a3
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-09-22 13:47:30 +0200

    Update altaera-model.sh

--- scripts/altaera-model.sh
+++ scripts/altaera-model.sh
@@ -8,30 +8,36 @@ TITLE="AI Model Selection"
 MENU="Choose a model to download:"
 
 OPTIONS=(1 "[...] Go Back"
-         2 "Auto-Detection (RWKV-4-World-Claude-for-Mobile) - [Default]"
-	 3 "RWKV-4-World-Claude-for-Mobile [...]"
-         4 "RWKV-4-World [...]"
-	 5 "KobbleTinyV2-1.1B [...]"
-	 6 "KobbleTinyV2-1.1B_iMatrix [...]"
-	 7 "Tiny-Vicuna-1B [...]"
-  	 8 "TinyDolphin-2.8-1.1b-imatrix [...]"
-  	 9 "TinyDolphin-2.8.2-1.1B-laser [...]"
-	 10 "TinyLlama-1.1B-Chat-v1.0 [...]"
-	 11 "Llama-3-8B-Instruct [...]"
-	 12 "Llama-2-7B-Chat [...]"
-	 13 "Mistral-7B-Instruct [...]"
-	 14 "Vicuna-7B-v1.5 [...]"
-	 15 "Mamba [...]"
-	 16 "Phi-3-mini-4k-instruct [...]"
-	 17 "Phi-SoSerious-Mini-V1 [...]"
-	 18 "Phi-SoSerious-Mini-V1-imatrix [...]"
-  	 19 "Phi-2-DPO-GGUF [...]"
-  	 20 "Yi-1.5-6B-Chat [...]"
-      	 21 "Gemma-2-2b-it [...]"
-  	 22 "Gemma-2-2b-it-abliterated [...]"
-  	 23 "Gemma-2-9b-it [...]"
-  	 24 "Gemma-2B-it [...]"
-  	 25 "Gemma-7B-it [...]")
+	 2 "RWKV-6-Finch-1B6 [...]"
+	 3 "RWKV-6-Finch-3B [...]"
+	 4 "RWKV-6-Finch-7B [...]"
+	 5 "RWKV-6-Finch-14B [...]"
+	 6 "RWKV-6-World-1B6 [...]"
+	 7 "RWKV-6-World-3B [...]"
+	 8 "RWKV-6-World-7B [...]"
+	 9 "RWKV-4-World-Claude-for-Mobile [...]"
+         10 "RWKV-4-World [...]"
+	 11 "KobbleTinyV2-1.1B [...]"
+	 12 "KobbleTinyV2-1.1B_iMatrix [...]"
+	 13 "Tiny-Vicuna-1B [...]"
+  	 14 "TinyDolphin-2.8-1.1b-imatrix [...]"
+  	 15 "TinyDolphin-2.8.2-1.1B-laser [...]"
+	 16 "TinyLlama-1.1B-Chat-v1.0 [...]"
+	 17 "Llama-3-8B-Instruct [...]"
+	 18 "Llama-2-7B-Chat [...]"
+	 19 "Mistral-7B-Instruct [...]"
+	 20 "Vicuna-7B-v1.5 [...]"
+	 21 "Mamba [...]"
+	 22 "Phi-3-mini-4k-instruct [...]"
+	 23 "Phi-SoSerious-Mini-V1 [...]"
+	 24 "Phi-SoSerious-Mini-V1-imatrix [...]"
+  	 25 "Phi-2-DPO-GGUF [...]"
+  	 26 "Yi-1.5-6B-Chat [...]"
+      	 27 "Gemma-2-2b-it [...]"
+  	 28 "Gemma-2-2b-it-abliterated [...]"
+  	 29 "Gemma-2-9b-it [...]"
+  	 30 "Gemma-2B-it [...]"
+  	 31 "Gemma-7B-it [...]")
 
 
 CHOICE=$(dialog --clear \
@@ -45,135 +51,126 @@ CHOICE=$(dialog --clear \
 clear
 case $CHOICE in
         1)
-            './AltaeraAI/altaera-manage_models.sh'
-	    ;;
+  		'./AltaeraAI/altaera-manage_models.sh'
+  		;;
+
         2)
-            echo "You chose 'Auto-detection - [Default]'"
-            cd '/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/altaera/root/kcpp-ae/models'
-            rm -rf 'RWKV-model.bin'
-            rm -rf 'model.bin'
-            
-   if [ $(free -h | awk '/Mem\:/ { print $2 }'
-) = "15Gi" ]; then
-	wget https://huggingface.co/ThinkThroughLabs/rwkv-claude-for-mobile-v4-world-1.5b-16k-ggml-quantized/resolve/main/q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -O 'q8_0-RWKV-for-mobile-4-world-1.5B-ctx16k.bin' -q --show-progress
-	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
-) = "11Gi" ]; then
-	wget https://huggingface.co/ThinkThroughLabs/rwkv-claude-for-mobile-v4-world-1.5b-16k-ggml-quantized/resolve/main/q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -O 'q8_0-RWKV-for-mobile-4-world-1.5B-ctx16k.bin' -q --show-progress
-	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
-) = "7Gi" ]; then
-	wget https://huggingface.co/ThinkThroughLabs/rwkv-claude-for-mobile-v4-world-1.5b-16k-ggml-quantized/resolve/main/q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -O 'q8_0-RWKV-for-mobile-4-world-1.5B-ctx16k.bin' -q --show-progress
-	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
-) = "5Gi" ]; then
-	wget https://huggingface.co/ThinkThroughLabs/rwkv-claude-for-mobile-v4-world-1.5b-16k-ggml-quantized/resolve/main/q5_1-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -O 'q5_1-RWKV-for-mobile-4-world-1.5B-ctx16k.bin' -q --show-progress
-	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
-) = "3Gi" ]; then
-	wget https://huggingface.co/ThinkThroughLabs/rwkv-claude-for-mobile-v4-world-1.5b-16k-ggml-quantized/resolve/main/q4_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -O 'q4_0-RWKV-for-mobile-4-world-1.5B-ctx16k.bin' -q --show-progress
-	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
-) = "2Gi" ]; then
-	wget https://huggingface.co/ThinkThroughLabs/rwkv-4-world-ggml/resolve/main/f32-RWKV-4-World-0.1B-v1-20230520-ctx4096.bin -O 'f32-RWKV-4-World-0.1B-v1-ctx4096.bin' -q --show-progress
-	elif [ $(free -h | awk '/Mem\:/ { print $2 }'
-) = "1Gi" ]; then
-	wget https://huggingface.co/ThinkThroughLabs/rwkv-4-world-ggml/resolve/main/f16-RWKV-4-World-0.1B-v1-20230520-ctx4096.bin -O 'f16-RWKV-4-World-0.1B-v1-ctx4096.bin' -q --show-progress
-  else
-	wget https://huggingface.co/ThinkThroughLabs/rwkv-claude-for-mobile-v4-world-1.5b-16k-ggml-quantized/resolve/main/q8_0-RWKV-for-mobile-4-world-1.5B-20230906-ctx16k.bin -O 'q8_0-RWKV-for-mobile-4-world-1.5B-ctx16k.bin' -q --show-progress
-fi
-            cd /root
-            clear
-            echo "
+  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-rwkv-6-finch-1b6'
+    		;;
 
-
-            You have succesfully changed the model to 'Auto-detection - [Default]'
-            "
-            ;;
         3)
-  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-rwkv-4-world-claude-for-mobile.sh'
+  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-rwkv-6-finch-3b'
     		;;
       
         4)
-  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-rwkv-4.sh'
+  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-rwkv-6-finch-7b'
     		;;
-      
+
         5)
-  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-kobbletiny.sh'
+  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-rwkv-6-finch-14b'
     		;;
-      
+
         6)
-  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-kobbletiny-imatrix.sh'
+  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-rwkv-6-world-1b6'
     		;;
-      
+
         7)
-  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-tiny-vicuna-1b.sh'
+  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-rwkv-6-world-3b'
     		;;
-      
+
         8)
-  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-tinydolphin-2.8-1.1b-imatrix.sh'
+  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-rwkv-6-world-7b'
     		;;
       
         9)
-  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-tinydolphin-2.8.2-1.1b-laser.sh'
+  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-rwkv-4-world-claude-for-mobile.sh'
     		;;
       
         10)
-  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-tinyllama-1.1b-chat-v1.0.sh'
+  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-rwkv-4.sh'
     		;;
       
         11)
+  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-kobbletiny.sh'
+    		;;
+      
+        12)
+  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-kobbletiny-imatrix.sh'
+    		;;
+      
+        13)
+  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-tiny-vicuna-1b.sh'
+    		;;
+      
+        14)
+  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-tinydolphin-2.8-1.1b-imatrix.sh'
+    		;;
+      
+        15)
+  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-tinydolphin-2.8.2-1.1b-laser.sh'
+    		;;
+      
+        16)
+  		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-tinyllama-1.1b-chat-v1.0.sh'
+    		;;
+      
+        17)
   		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-llama-3-8b-instruct.sh'
     		;;
       
-    	12)	
+    	18)	
      		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-llama-2-7b-chat.sh'
     		;;
       
-    	13)
+    	19)
      		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-mistral-7b-instruct-v0.2.sh'
     		;;
       
-    	14)
+    	20)
      		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-vicuna-7b-v1.5.sh'
     		;;
 
-    	15)
+    	21)
      		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-mamba.sh'
     		;;
       
-    	16)
+    	22)
      		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-phi-3-mini-4k-instruct.sh'
     		;;
 
-    	17)
+    	23)
      		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-phi-soserious-mini-v1.sh'
     		;;
       
-    	18)
+    	24)
      		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-phi-soserious-mini-v1-imatrix.sh'
     		;;
 
-    	19)
+    	25)
      		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-phi-2-dpo.sh'
     		;;
       
-    	20)
+    	26)
      		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-yi-1.5-6b-chat.sh'
     		;;
       
-    	21)
+    	27)
      		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-gemma-2-2b-it.sh'
     		;;
 
-    	22)
+    	28)
      		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-gemma-2-2b-it-abliterated.sh'
     		;;
 
-    	23)
+    	29)
      		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-gemma-2-9b-it.sh'
     		;;
       
-    	24)
+    	30)
      		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-gemma-2b-it.sh'
     		;;
       
-    	25)
+    	31)
      		bash '/data/data/com.termux/files/home/AltaeraAI/altaera-model-gemma-7b-it.sh'
     		;;
       
