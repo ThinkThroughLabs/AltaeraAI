54582e093bd98ada11e1a50b3712fb39e3c54bce
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2024-10-13 12:41:34 +0200

    Update altaera-changelog_cli.sh

--- scripts/altaera-changelog_cli.sh
+++ scripts/altaera-changelog_cli.sh
@@ -3,6 +3,7 @@
 dialog --backtitle "AltaeraAI" \
 --title "AltaeraAI - Changelog [CLI]" \
 --msgbox '
+    * v5.7.8 - updated the pre-packaged KoboldCpp to v1.76
     * v5.7.7 - added RWKV-6-Finch and RWKV-6-World weights to the list of models
     * added option to compile experimental KoboldCpp
     * removed OpenBLAS, which was replaced by the "llamafile" library - it is now set by default
