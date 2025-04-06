50866d3ca560aeefef4f8da953892422a877bc14
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2025-04-06 17:23:06 +0200

    Update altaera_no-check.sh

--- scripts/altaera_no-check.sh
+++ scripts/altaera_no-check.sh
@@ -18,17 +18,16 @@ OPTIONS=(1 "Start AltaeraAI [...]"
          7 "Benchmark AI Models [KoboldCpp]"
          8 "Manage AI Models 🤖 [...]"
          9 "Manage stories (Backup/Restore) [...]"
-         10 "(!) Convert/Quantize GGUF Models [...]"
-         11 "Change language 🌐"
-         12 "Change KoboldCpp Settings [...]"
-         13 "Change AltaeraAI Settings [...]"
-         14 "ngrok Secure Tunnelling [...]"
-         15 "Reinstall (fix) AltaeraAI [...]"
-         16 "Uninstall AltaeraAI"
-         17 "Customer Support [...]"
-         18 "Visit altaera.ai [URL]"
-         19 "Donate 💸 [URL]"
-         20 "Exit")
+         10 "Change language 🌐"
+         11 "Change KoboldCpp Settings [...]"
+         12 "Change AltaeraAI Settings [...]"
+         13 "ngrok Secure Tunnelling [...]"
+         14 "Reinstall (fix) AltaeraAI [...]"
+         15 "Uninstall AltaeraAI"
+         16 "Customer Support [...]"
+         17 "Visit altaera.ai [URL]"
+         18 "Donate 💸 [URL]"
+         19 "Exit")
          
 
 CHOICE=$(dialog --clear \
@@ -226,96 +225,45 @@ echo "Logging into PRoot Distro ...
             ;;
         10)
             clear
-            echo "
-
-        ██████████
-       ██ █
-      ██  █
-     ██   █
-     █    ████████
-    ██    █
-   ████████
-  ██      █
-  █       ████████
-
-  AltaeraAI - v6.0.0
-
-    by ThinkThrough
-
-
-  "
-
-  echo "Did you know...
-
-  " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-
-  	function random_message() {
-    # Array of random messages
-    messages=("that KobbleTiny is the world's sweetest child?" "that KobbleTiny is concedo's designed mind?")
-
-    # Generate a random index
-    index=$(( RANDOM % ${#messages[@]} ))
-
-    # Get the random message
-    message=${messages[$index]}
-
-    # Print the random message
-    echo $message | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-}
-
-# Call the function
-random_message
-
-echo "________________________________________________________________
-" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-
-echo "Logging into PRoot Distro ...
-
-" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-
-            proot-distro login altaera -- ./altaera-cq_gguf_models.sh
-            ;;
-        11)
-            clear
             './AltaeraAI/altaera-lang.sh'
             './AltaeraAI/altaera.sh'
             ;;
-        12)
+        11)
             clear
             './AltaeraAI/altaera-koboldcpp_settings.sh'
             './AltaeraAI/altaera.sh'
             ;;
-        13)
+        12)
             clear
             './AltaeraAI/altaera-settings.sh'
             './AltaeraAI/altaera.sh'
             ;;
-        14)
+        13)
             clear
             './AltaeraAI/altaera-ngrok.sh'
             './AltaeraAI/altaera.sh'
             ;;
-        15)
+        14)
             clear
             './AltaeraAI/altaera-reinstall.sh'
             ;;
-        16)
+        15)
             clear
             './AltaeraAI/altaera-uninstall.sh'
             ;;
-        17)
+        16)
             clear
             './AltaeraAI/altaera-customer_support.sh'
             ;;
-        18)
+        17)
             clear
             termux-open-url 'https://altaera.ai/'
             ;;
-        19)
+        18)
             clear
             termux-open-url 'https://altaera.ai/donate/'
             ;;
-        20)
+        19)
             clear
             exit
 esac
