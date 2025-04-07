e6b210aaf092f3de306a4c24b1184e5cf3491486
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2025-04-07 11:51:14 +0200

    Update altaera_install_pt-4.sh

--- scripts/altaera_install_pt-4.sh
+++ scripts/altaera_install_pt-4.sh
@@ -208,6 +208,8 @@ rm -rf 'chat-with-bob.txt'
 rm -rf 'altaera_install_pt-3.sh'
 rm -rf 'altaera_install_pt-4.sh'
 rm -rf 'altaera_install-architecture.sh'
+rm '/data/data/com.termux/files/home/.dialogrc'
+cp '/data/data/com.termux/files/home/AltaeraAI-tmp'/termux-default/dialogrc' '/data/data/com.termux/files/home/.dialogrc'
 } &> /dev/null 2>&1;
 
 clear
