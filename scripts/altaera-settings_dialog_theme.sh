473b2b3957ed3575bd8a99f1f8e113e971e3d28c
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2025-04-07 11:58:10 +0200

    Update altaera-settings_dialog_theme.sh

--- scripts/altaera-settings_dialog_theme.sh
+++ scripts/altaera-settings_dialog_theme.sh
@@ -5,7 +5,7 @@ WIDTH=60
 CHOICE_HEIGHT=7
 BACKTITLE="AltaeraAI - Settings"
 TITLE="Dialog Theme [MENU]"
-MENU="Would you like to turn ON, or turn OFF black dialog theme [MENU]?"
+MENU="Would you like to turn ON, or turn OFF the customized AltaeraAI dialog theme [MENU]?"
 
 OPTIONS=(1 "[...] Go Back"
          2 "Turn ON"
@@ -27,26 +27,16 @@ case $CHOICE in
 	    ;;
         2)
             {
-            if [ -f "/data/data/com.termux/files/home/.dialogrc" ]; then
-cp '/data/data/com.termux/files/home/.dialogrc' '/data/data/com.termux/files/home/AltaeraAI-tmp'/termux-default/dialogrc'
 rm '/data/data/com.termux/files/home/.dialogrc'
 wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/refs/heads/main/termux/dialogrc -O '.dialogrc' -P '/data/data/com.termux/files/home'
-elif [ ! -f "/data/data/com.termux/files/home/.dialogrc" ]; then
-dialog --create-rc ~/.dialogrc
-cp '/data/data/com.termux/files/home/.dialogrc' '/data/data/com.termux/files/home/AltaeraAI-tmp'/termux-default/dialogrc'
-rm '/data/data/com.termux/files/home/.dialogrc'
-wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/refs/heads/main/termux/dialogrc -O '.dialogrc' -P '/data/data/com.termux/files/home'
-fi
-fi
 proot-distro login altaera -- ./dialog_theme-on.sh &
 exit
 } &> /dev/null 2>&1;
             ;;
         3)
             {
-            dialog --create-rc ~/.dialogrc
-sed -i '/^screen_color = (CYAN,BLACK,ON)/d' ~/.dialogrc
-echo "screen_color = (CYAN,BLUE,ON)" >> ~/.dialogrc
+rm '/data/data/com.termux/files/home/.dialogrc'
+cp '/data/data/com.termux/files/home/AltaeraAI-tmp/termux-default/dialogrc' '/data/data/com.termux/files/home/.dialogrc'
 proot-distro login altaera -- ./dialog_theme-off.sh &
 exit
 } &> /dev/null 2>&1;
