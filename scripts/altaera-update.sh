c702f040678f3468586d30ffd1b779eed9d0196f
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2025-04-09 11:39:50 +0200

    Update altaera-update.sh

--- scripts/altaera-update.sh
+++ scripts/altaera-update.sh
@@ -12,7 +12,6 @@ OPTIONS=(1 "Update with pre-packaged KoboldCpp [Faster]"
          3 "Update with building your own Experimental KoboldCpp [Slower]"
          4 "Update shell files only")
 
-
 CHOICE=$(dialog --clear \
                 --backtitle "$BACKTITLE" \
                 --title "$TITLE" \
@@ -22,12 +21,20 @@ CHOICE=$(dialog --clear \
                 2>&1 >/dev/tty)
 
 clear
-case $CHOICE in
 
+# Check function for dynamically updating check marks
+check_status() {
+    echo -n "$1 [ ... ]"
+    tput el
+    sleep 1  # Simulate task time
+    echo -n "$1 [ ✔ ]"
+    echo
+}
+
+case $CHOICE in
         1)
             clear
             echo "
-
 ████████████████████████
 ██                    ██
 ██        ██████████  ██
@@ -45,50 +52,35 @@ case $CHOICE in
   AltaeraAI - v6.0.0
 
     by ThinkThrough
-
-
   "
+            echo "Did you know..."
 
-  echo "Did you know...
+            # Random message function
+            function random_message() {
+                messages=("that KobbleTiny is the world's sweetest child?" "that KobbleTiny is concedo's designed mind?")
+                index=$(( RANDOM % ${#messages[@]} ))
+                message=${messages[$index]}
+                echo $message
+            }
+            random_message
 
-  " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-
-  	function random_message() {
-    # Array of random messages
-    messages=("that KobbleTiny is the world's sweetest child?" "that KobbleTiny is concedo's designed mind?")
-
-    # Generate a random index
-    index=$(( RANDOM % ${#messages[@]} ))
-
-    # Get the random message
-    message=${messages[$index]}
-
-    # Print the random message
-    echo $message | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-}
-
-# Call the function
-random_message
-
-echo "________________________________________________________________
-" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-
-            echo "Initializing update ...
-
-            " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+            # Updating steps
+            check_status "Initializing update"
             {
-            rm -rf 'altaera-update_content.sh'
-            wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-update_content-fast.sh -O 'altaera-update_content.sh'
-            chmod a+x 'altaera-update_content.sh'
-            } &> /dev/null 2>&1;
+                rm -rf 'altaera-update_content.sh'
+                wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-update_content-fast.sh -O 'altaera-update_content.sh'
+                chmod a+x 'altaera-update_content.sh'
+            } &> /dev/null 2>&1
+            check_status "Downloading update content"
             bash 'altaera-update_content.sh'
+            check_status "Applying update"
             clear
             bash 'AltaeraAI/altaera-updated_successfully.sh'
             ;;
+
         2)
             clear
             echo "
-
 ████████████████████████
 ██                    ██
 ██        ██████████  ██
@@ -106,50 +98,35 @@ echo "________________________________________________________________
   AltaeraAI - v6.0.0
 
     by ThinkThrough
-
-
   "
+            echo "Did you know..."
 
-    echo "Did you know...
+            # Random message function
+            function random_message() {
+                messages=("that KobbleTiny is the world's sweetest child?" "that KobbleTiny is concedo's designed mind?")
+                index=$(( RANDOM % ${#messages[@]} ))
+                message=${messages[$index]}
+                echo $message
+            }
+            random_message
 
-  " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-
-  	function random_message() {
-    # Array of random messages
-    messages=("that KobbleTiny is the world's sweetest child?" "that KobbleTiny is concedo's designed mind?")
-
-    # Generate a random index
-    index=$(( RANDOM % ${#messages[@]} ))
-
-    # Get the random message
-    message=${messages[$index]}
-
-    # Print the random message
-    echo $message | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-}
-
-# Call the function
-random_message
-
-echo "________________________________________________________________
-" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-
-            echo "Initializing update ...
-
-            " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+            # Updating steps
+            check_status "Initializing update"
             {
-            rm -rf 'altaera-update_content.sh'
-            wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-update_content-slow.sh -O 'altaera-update_content.sh'
-            chmod a+x 'altaera-update_content.sh'
-            } &> /dev/null 2>&1;
+                rm -rf 'altaera-update_content.sh'
+                wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-update_content-slow.sh -O 'altaera-update_content.sh'
+                chmod a+x 'altaera-update_content.sh'
+            } &> /dev/null 2>&1
+            check_status "Downloading update content"
             bash 'altaera-update_content.sh'
+            check_status "Applying update"
             clear
             bash 'AltaeraAI/altaera-updated_successfully.sh'
             ;;
+
         3)
             clear
             echo "
-
 ████████████████████████
 ██                    ██
 ██        ██████████  ██
@@ -167,50 +144,35 @@ echo "________________________________________________________________
   AltaeraAI - v6.0.0
 
     by ThinkThrough
-
-
   "
+            echo "Did you know..."
 
-    echo "Did you know...
+            # Random message function
+            function random_message() {
+                messages=("that KobbleTiny is the world's sweetest child?" "that KobbleTiny is concedo's designed mind?")
+                index=$(( RANDOM % ${#messages[@]} ))
+                message=${messages[$index]}
+                echo $message
+            }
+            random_message
 
-  " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-
-  	function random_message() {
-    # Array of random messages
-    messages=("that KobbleTiny is the world's sweetest child?" "that KobbleTiny is concedo's designed mind?")
-
-    # Generate a random index
-    index=$(( RANDOM % ${#messages[@]} ))
-
-    # Get the random message
-    message=${messages[$index]}
-
-    # Print the random message
-    echo $message | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-}
-
-# Call the function
-random_message
-
-echo "________________________________________________________________
-" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-
-            echo "Initializing update ...
-
-            " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+            # Updating steps
+            check_status "Initializing update"
             {
-            rm -rf 'altaera-update_content.sh'
-            wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/refs/heads/main/scripts/altaera-update_content-slow_experimental.sh -O 'altaera-update_content.sh'
-            chmod a+x 'altaera-update_content.sh'
-            } &> /dev/null 2>&1;
+                rm -rf 'altaera-update_content.sh'
+                wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/refs/heads/main/scripts/altaera-update_content-slow_experimental.sh -O 'altaera-update_content.sh'
+                chmod a+x 'altaera-update_content.sh'
+            } &> /dev/null 2>&1
+            check_status "Downloading update content"
             bash 'altaera-update_content.sh'
+            check_status "Applying update"
             clear
             bash 'AltaeraAI/altaera-updated_successfully.sh'
             ;;
+
         4)
             clear
             echo "
-
 ████████████████████████
 ██                    ██
 ██        ██████████  ██
@@ -228,45 +190,29 @@ echo "________________________________________________________________
   AltaeraAI - v6.0.0
 
     by ThinkThrough
-
-
   "
+            echo "Did you know..."
 
-    echo "Did you know...
+            # Random message function
+            function random_message() {
+                messages=("that KobbleTiny is the world's sweetest child?" "that KobbleTiny is concedo's designed mind?")
+                index=$(( RANDOM % ${#messages[@]} ))
+                message=${messages[$index]}
+                echo $message
+            }
+            random_message
 
-  " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-
-  	function random_message() {
-    # Array of random messages
-    messages=("that KobbleTiny is the world's sweetest child?" "that KobbleTiny is concedo's designed mind?")
-
-    # Generate a random index
-    index=$(( RANDOM % ${#messages[@]} ))
-
-    # Get the random message
-    message=${messages[$index]}
-
-    # Print the random message
-    echo $message | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-}
-
-# Call the function
-random_message
-
-echo "________________________________________________________________
-" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-
-            echo "Initializing update ...
-
-            " | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
+            # Updating steps
+            check_status "Initializing update"
             {
-            rm -rf 'altaera-update_content.sh'
-            wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-update_content-scripts.sh -O 'altaera-update_content.sh'
-            chmod a+x 'altaera-update_content.sh'
-            } &> /dev/null 2>&1;
+                rm -rf 'altaera-update_content.sh'
+                wget https://raw.githubusercontent.com/ThinkThroughLabs/AltaeraAI/main/scripts/altaera-update_content-scripts.sh -O 'altaera-update_content.sh'
+                chmod a+x 'altaera-update_content.sh'
+            } &> /dev/null 2>&1
+            check_status "Downloading update content"
             bash 'altaera-update_content.sh'
+            check_status "Applying update"
             clear
             bash 'AltaeraAI/altaera-updated_successfully.sh'
             ;;
-
 esac
