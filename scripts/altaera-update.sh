4a407b8dacd9cbecbd58e31ff9d796af3d132b6e
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2025-04-22 20:18:34 +0200

    Update altaera-update.sh

--- scripts/altaera-update.sh
+++ scripts/altaera-update.sh
@@ -25,8 +25,8 @@ clear
 # Spinner that runs while a given PID is alive
 spin() {
     local pid="$1"
-    local delay=0.1
-    local chars='|/-\\'
+    local delay=0.2
+    local chars='⠁ ⠂ ⠄ ⡀ ⢀ ⠠ ⠐ ⠈'
     tput civis
     while kill -0 "$pid" 2>/dev/null; do
         for ((i=0; i<${#chars}; i++)); do
