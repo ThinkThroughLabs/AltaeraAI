97505ff84360ad1ee9abb412e201a82eae28ffca
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2025-04-30 12:40:40 +0200

    Update altaera_pre-launch.sh

--- scripts/altaera-proot/altaera_pre-launch.sh
+++ scripts/altaera-proot/altaera_pre-launch.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+{
+rm 'AltaeraAI-tmp/termux-default/dialogrc'
+cp '/data/data/com.termux/files/home/.dialogrc' '/data/data/com.termux/files/home/AltaeraAI-tmp/termux-default/dialogrc'
+rm '/data/data/com.termux/files/home/.dialogrc'
+cp '/data/data/com.termux/files/home/AltaeraAI/termux/dialogrc/dialogrc' '/data/data/com.termux/files/home/.dialogrc'
+} &> /dev/null 2>&1;
+
 HEIGHT=20
 WIDTH=60
 CHOICE_HEIGHT=24
@@ -55,3 +62,8 @@ case $CHOICE in
 	    fi
       
 esac
+
+{
+rm '/data/data/com.termux/files/home/.dialogrc'
+cp '/data/data/com.termux/files/home/AltaeraAI-tmp/termux-default/dialogrc' '/data/data/com.termux/files/home/.dialogrc'
+} &> /dev/null 2>&1;
