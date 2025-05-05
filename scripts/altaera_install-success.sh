31c3fa97755aefc46061f2c27dd29ad0922a954b
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2025-05-05 15:38:10 +0200

    Create altaera_install-success.sh

---
+++ scripts/altaera_install-success.sh
@@ -0,0 +1,26 @@
+# Typing animation function
+type_text_centered() {
+    text="$1"
+    delay="${2:-0.05}"
+    term_width=$(tput cols)
+    padding=$(( (term_width - ${#text}) / 2 ))
+
+    printf "%*s" "$padding" ""
+    for ((i=0; i<${#text}; i++)); do
+        printf "%s" "${text:$i:1}"
+        sleep "$delay"
+    done
+    echo
+}
+
+# Final success message
+clear
+term_width=$(tput cols)
+success_msg="✔ Installation completed successfully."
+padding=$(( (term_width - ${#success_msg}) / 2 ))
+echo -e "\n"
+printf "%*s\033[1;32m%s\033[0m\n\n" "$padding" "" "$success_msg"
+
+# Animated, centered instruction
+type_text_centered "To start, please type in 'ae'."
+;;
