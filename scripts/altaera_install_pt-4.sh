f0a6e674fc18a69276b8eb48ca89eb58591d9688
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2025-05-05 13:23:19 +0200

    Update altaera_install_pt-4.sh

--- scripts/altaera_install_pt-4.sh
+++ scripts/altaera_install_pt-4.sh
@@ -485,22 +485,6 @@ else
 :
 fi
 
-echo "**********INSTALLATION COMPLETED**********
-" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-echo "________________________________________________________________
-" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-echo "
-      !!!  - Please, pull down the notification bar, expand 'Termux' and press 'Exit'  !!!
-      !!!  - Or, press 'CTRL+D' twice.                                                !!!
-
-      --- Then, open Termux and:
-      
-- To start, please type in 'ae'.
-
-            [Please, ignore the 'ERROR' below if it shows up]"
-echo "________________________________________________________________
-" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols);
-
 exit
 
 exec <&-
