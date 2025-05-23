ce5b4fcf3d2d0a19ae6b9e7f12e7ee0515c258ce
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2025-05-23 13:58:14 +0200

    Update altaera-changelog_cli.sh

--- scripts/altaera-changelog_cli.sh
+++ scripts/altaera-changelog_cli.sh
@@ -3,7 +3,9 @@
 dialog --backtitle "AltaeraAI" \
 --title "AltaeraAI - Changelog [CLI]" \
 --msgbox '
-    * v6.1.0 - updated the pre-packaged KoboldCpp to v1.91
+    * v6.1.0 - partially revamped UI, as part of a bigger code rebuild and refactoring; changed theme, added animations, improved functionalities, fixed bugs, added the ability to stop KoboldCpp inference by pressing "q"
+    * added RWKV-G1 and Gemma-3 weights to the list of models
+    * v6.0.6 - updated the pre-packaged KoboldCpp to v1.91
     * v6.0.5 - updated the pre-packaged KoboldCpp to v1.90.2
     * v6.0.4 - updated the pre-packaged KoboldCpp to v1.89
     * v6.0.3 - fixed the link to RWKV7-Goose-World3-2.9B
