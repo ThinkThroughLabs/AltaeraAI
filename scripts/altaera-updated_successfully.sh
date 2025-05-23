1ee8a029c9403ae674f22a140d7738ff34080ea7
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2025-05-23 13:58:41 +0200

    Update altaera-updated_successfully.sh

--- scripts/altaera-updated_successfully.sh
+++ scripts/altaera-updated_successfully.sh
@@ -7,12 +7,13 @@ dialog --backtitle "AltaeraAI" \
 
 Recent changes:
 
-    * v6.1.0 - updated the pre-packaged KoboldCpp to v1.91
+    * v6.1.0 - partially revamped UI, as part of a bigger code rebuild and refactoring; changed theme, added animations, improved functionalities, fixed bugs, added the ability to stop KoboldCpp inference by pressing "q"
+    * added RWKV-G1 and Gemma-3 weights to the list of models
+    * v6.0.6 - updated the pre-packaged KoboldCpp to v1.91
     * v6.0.5 - updated the pre-packaged KoboldCpp to v1.90.2
     * v6.0.4 - updated the pre-packaged KoboldCpp to v1.89
     * v6.0.3 - fixed the link to RWKV7-Goose-World3-2.9B
     * v6.0.2 - updated the pre-packaged KoboldCpp to experimental/v1.88 — the reason being that the experimental branch included a fix for RWKV related to a broken recurrent cache component in upstream LlamaCpp
-    * v6.0.1 - added RWKV7-Goose-World weights to the list of models
 ' 25 40
 
 #bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
