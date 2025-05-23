b662ac34aea0999c80e8f4e3583ea1d0129e8f12
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2025-05-23 16:49:37 +0200

    Update altaera-manage_models.sh

--- scripts/altaera-manage_models.sh
+++ scripts/altaera-manage_models.sh
@@ -26,7 +26,7 @@ CHOICE=$(dialog --clear \
 clear
 case $CHOICE in
         1)
-            './AltaeraAI/altaera_no-check.sh'
+            bash '/data/data/com.termux/files/home/AltaeraAI/altaera_no-check.sh'
             exit 0
 	    ;;
         2)
