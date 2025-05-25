a0b2820db43baf1b1a12129037bac65537d9c4fd
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2025-05-25 14:03:10 +0200

    Update altaera-updated_successfully.sh

--- scripts/altaera-updated_successfully.sh
+++ scripts/altaera-updated_successfully.sh
@@ -7,13 +7,13 @@ dialog --backtitle "AltaeraAI" \
 
 Recent changes:
 
+    * v6.1.2 - updated the pre-packaged KoboldCpp to v1.92.1
     * v6.1.1 - added the ability to cancel model downloading by pressing "c". The cancelled file will now be deleted automatically
     * v6.1.0 - partially revamped UI, as part of a bigger code rebuild and refactoring; changed theme, added animations, improved functionalities, fixed bugs, added the ability to stop KoboldCpp inference by pressing "q"
     * added RWKV-G1 and Gemma-3 weights to the list of models
     * v6.0.6 - updated the pre-packaged KoboldCpp to v1.91
     * v6.0.5 - updated the pre-packaged KoboldCpp to v1.90.2
     * v6.0.4 - updated the pre-packaged KoboldCpp to v1.89
-    * v6.0.3 - fixed the link to RWKV7-Goose-World3-2.9B
 ' 25 40
 
 #bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
