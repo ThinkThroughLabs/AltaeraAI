2295006837ef0524252a2338009eab934968e0b0
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2025-05-27 11:32:57 +0200

    Update altaera-updated_successfully.sh

--- scripts/altaera-updated_successfully.sh
+++ scripts/altaera-updated_successfully.sh
@@ -7,13 +7,13 @@ dialog --backtitle "AltaeraAI" \
 
 Recent changes:
 
+    * v6.1.3 - delayed browser launch (termux-open-url) until KoboldCpp back-end is ready. This ensures the front-end opens only after the model and server are fully loaded, while preserving live output display and UI layout
     * v6.1.2 - updated the pre-packaged KoboldCpp to v1.92.1
     * v6.1.1 - added the ability to cancel model downloading by pressing "c". The cancelled file will now be deleted automatically
     * v6.1.0 - partially revamped UI, as part of a bigger code rebuild and refactoring; changed theme, added animations, improved functionalities, fixed bugs, added the ability to stop KoboldCpp inference by pressing "q"
     * added RWKV-G1 and Gemma-3 weights to the list of models
     * v6.0.6 - updated the pre-packaged KoboldCpp to v1.91
     * v6.0.5 - updated the pre-packaged KoboldCpp to v1.90.2
-    * v6.0.4 - updated the pre-packaged KoboldCpp to v1.89
 ' 25 40
 
 #bash '/data/data/com.termux/files/home/AltaeraAI/altaera.sh'
