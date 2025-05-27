861e9e0a7ebd5eaac49f4249e0502bee7aaf4708
Krystian Węgrzyniak <krystianwegrzyniak@tutanota.com>
2025-05-27 11:33:15 +0200

    Update altaera-changelog_cli.sh

--- scripts/altaera-changelog_cli.sh
+++ scripts/altaera-changelog_cli.sh
@@ -3,6 +3,7 @@
 dialog --backtitle "AltaeraAI" \
 --title "AltaeraAI - Changelog [CLI]" \
 --msgbox '
+    * v6.1.3 - delayed browser launch (termux-open-url) until KoboldCpp back-end is ready. This ensures the front-end opens only after the model and server are fully loaded, while preserving live output display and UI layout
     * v6.1.2 - updated the pre-packaged KoboldCpp to v1.92.1
     * v6.1.1 - added the ability to cancel model downloading by pressing "c". The cancelled file will now be deleted automatically
     * v6.1.0 - partially revamped UI, as part of a bigger code rebuild and refactoring; changed theme, added animations, improved functionalities, fixed bugs, added the ability to stop KoboldCpp inference by pressing "q"
